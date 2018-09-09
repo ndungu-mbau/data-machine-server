0ee02272b934b18523fb54b95304cd31fb4af816
GitomehBranson <gitomehbranson@gmail.com>
2018-09-09 04:30:09 +0300

    remove gcoud dependencies everywhere

--- src/config.js
+++ src/config.js
@@ -1,35 +1,18 @@
-import Datastore from '@google-cloud/datastore'
-import Storage from '@google-cloud/storage'
 
 const { DBURL = "mongodb://localhost:27017/bowdice" } = process.env
 
-const datastore = new Datastore({
-    projectId: 'sails-970',
-    keyFilename: './key.json',
-});
-
-const storage = new Storage({
-    projectId: 'sails-970',
-    keyFilename: './key.json',
-});
 
 const config = {
     production: {
         dbUrl: `mongodb://bowdice_server:0LwB6gsaS8VvKpkNL3Sma@ds219532.mlab.com:19532/bowdice`,
         hashingSecret: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmb28iOiJiYXIiLCJpYXQiOjE1MzQwMzU2NDF9.B20KQXUv3hP873OEbb0lTbb63HrkDjsHNQkRHAHkGJE",
-        datastore,
-        storage
     },
     development: {
         dbUrl: `${DBURL}`,
         hashingSecret: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9",
-        datastore,
-        storage
     },
     staging: {
-        dbUrl: `mongodb://bowdice_server:0LwB6gsaS8VvKpkNL3Sma@ds219532.mlab.com:19532/bowdice`,
-        datastore,
-        storage
+        dbUrl: `mongodb://bowdice_server:0LwB6gsaS8VvKpkNL3Sma@ds219532.mlab.com:19532/bowdice`
     }
 }
 
