3cff5665c11d5531a6a8eb25029090a47838facb
GitomehBranson <gitomehbranson@gmail.com>
2018-09-22 17:45:15 +0300

    linting and user password hashing password on both create and update

--- src/config.js
+++ src/config.js
@@ -1,25 +1,25 @@
 
-const { DBURL = "mongodb://localhost:27017/bowdice" } = process.env
+const { DBURL = 'mongodb://localhost:27017/bowdice' } = process.env;
 
 
 const config = {
-    production: {
-        db: {
-            name: "databank",
-            url: `mongodb://databank_server:n16dXUWGHiLT@ds149732.mlab.com:49732/databank`
-        },
-        hashingSecret: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmb28iOiJiYXIiLCJpYXQiOjE1MzQwMzU2NDF9.B20KQXUv3hP873OEbb0lTbb63HrkDjsHNQkRHAHkGJE",
+  production: {
+    db: {
+      name: 'databank',
+      url: 'mongodb://databank_server:n16dXUWGHiLT@ds149732.mlab.com:49732/databank',
     },
-    development: {
-        db: {
-            name: "besak",
-            url: `${DBURL}`
-        },
-        hashingSecret: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9",
+    hashingSecret: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmb28iOiJiYXIiLCJpYXQiOjE1MzQwMzU2NDF9.B20KQXUv3hP873OEbb0lTbb63HrkDjsHNQkRHAHkGJE',
+  },
+  development: {
+    db: {
+      name: 'besak',
+      url: `${DBURL}`,
     },
-    staging: {
-        dbUrl: `mongodb://bowdice_server:0LwB6gsaS8VvKpkNL3Sma@ds219532.mlab.com:19532/bowdice`
-    }
-}
+    hashingSecret: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9',
+  },
+  staging: {
+    dbUrl: 'mongodb://bowdice_server:0LwB6gsaS8VvKpkNL3Sma@ds219532.mlab.com:19532/bowdice',
+  },
+};
 
-export default config
+export default config;
--- src/graphql/mutations/users/entity.js
+++ src/graphql/mutations/users/entity.js
@@ -1,32 +1,36 @@
-import sha1 from 'sha1'
+import sha1 from 'sha1';
 
-const collection = "user"
+const collection = 'user';
 
 const create = async (args, { db, ObjectId }) => {
-  const entry = args[collection]
+  const entry = args[collection];
   Object.assign(entry, {
     _id: new ObjectId(),
-    destroyed: false
-  })
-  entry.password = sha1(password)
-  db.collection(collection).insertOne(entry)
-  entry.id = entry._id
-  return entry
+    destroyed: false,
+  });
+  entry.password = sha1(entry.password);
+  db.collection(collection).insertOne(entry);
+  entry.id = entry._id;
+  return entry;
 };
 
 const update = async (args, { db, ObjectId }) => {
-  const entry = args[collection]
-  return await db.collection(collection).updateOne({ _id: new ObjectId(entry.id) }, { $set: Object.assign({}, entry, { id: undefined }) })
+  const entry = args[collection];
+  if (entry.password) { entry.password = sha1(entry.password); }
+  return db.collection(collection)
+    .updateOne({ _id: new ObjectId(entry.id) }, {$set: Object.assign({}, entry, { id: undefined }) });
 };
 
 const destroy = async (args, { db, ObjectId }) => {
-  const entry = args[collection]
-  return await db.collection(collection).updateOne({ _id: new ObjectId(entry.id) }, { $set: { destroyed: true } })
+  const entry = args[collection];
+  return db.collection(collection)
+    .updateOne({ _id: new ObjectId(entry.id) }, { $set: { destroyed: true } });
 };
 
 const restore = async (args, { db, ObjectId }) => {
-  const entry = args[collection]
-  return await db.collection(collection).updateOne({ _id: new ObjectId(entry.id) }, { $set: { destroyed: false } })
+  const entry = args[collection];
+  return db.collection(collection)
+    .updateOne({ _id: new ObjectId(entry.id) }, { $set: { destroyed: false } });
 };
 
 export {
