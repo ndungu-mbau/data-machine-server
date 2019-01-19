ba7c65a8429361777e6c733c35c9b6b0e6ee6d2f
BransonGitomeh <sirbranson67@gmail.com>
2019-01-19 12:27:22 +0300

    fix db name

--- src/config.js
+++ src/config.js
@@ -5,7 +5,7 @@ const { DB_URL: url = 'mongodb://localhost:27017' } = process.env;
 const config = {
   production: {
     db: {
-      name: 'boowdice',
+      name: 'bowdice',
       url,
     },
     hashingSecret: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmb28iOiJiYXIiLCJpYXQiOjE1MzQwMzU2NDF9.B20KQXUv3hP873OEbb0lTbb63HrkDjsHNQkRHAHkGJE',
