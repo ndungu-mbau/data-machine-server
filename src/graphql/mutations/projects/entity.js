aae5756fe39b2ca052f3b091ad92d9b83fd6aed7
gitomehbranson <sirbranson67@gmail.com>
2019-01-27 14:34:27 +0300

    create items after project creation

--- src/graphql/mutations/projects/entity.js
+++ src/graphql/mutations/projects/entity.js
@@ -1,27 +1,58 @@
 const collection = "project"
 
 const create = async (args, { db, ObjectId }) => {
+
+  console.log(ObjectId)
   const entry = args[collection]
   Object.assign(entry, {
     _id: new ObjectId(),
     destroyed: false
   })
 
-  if (entry.questionnaire === 'new') {
-    const questionnaire = {
-      _id: new ObjectId,
-      name: entry.name,
-      project: entry._id.toString(),
-      destroyed: false
-    }
-
-    console.log(questionnaire)
-
-    await db.collection('questionnaire').insertOne(questionnaire)
-    entry.questionnaire = questionnaire._id
+  const questionnaire = {
+    _id: new ObjectId(),
+    name: entry.name,
+    project: entry._id.toString(),
+    destroyed: false
   }
-  db.collection(collection).insertOne(entry)
+
+  await db.collection('questionnaire').insertOne(questionnaire)
+  entry.questionnaire = questionnaire._id
+
+  await db.collection(collection).insertOne(entry)
   entry.id = entry._id
+
+  const page = {
+    _id: new ObjectId(),
+    name: 'Sample page',
+    questionnaire: questionnaire._id.toString(),
+    destroyed: false
+  }
+
+  // create questionnire things
+  await db.collection('page').insertOne(page);
+
+  const group = {
+    _id: new ObjectId(),
+    name: 'Sample group',
+    page: page._id.toString(),
+    destroyed: false
+  }
+
+  // create questionnire things
+  await db.collection('group').insertOne(group);
+
+  const question = {
+    _id: new ObjectId(),
+    type: 'instruction',
+    placeholder: 'Sample instruction',
+    group: group._id.toString(),
+    destroyed: false
+  }
+
+  // create questionnire things
+  await db.collection('question').insertOne(question);
+
   return entry
 };
 
